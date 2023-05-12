eb2265e575aa98558bb168b1e919378c62ffc3ae
Dilek Baykara <baykara.d@gmail.com>
2023-05-12 19:05:19 -0400

    adding bar grid to forecast

--- weather-app/src/components/Forecast.tsx
+++ weather-app/src/components/Forecast.tsx
@@ -1,3 +1,4 @@
+import { Hourglass } from "lucide-react";
 import React, { useEffect, useState } from "react";
 
 function Forecast(props: {
@@ -6,7 +7,7 @@ function Forecast(props: {
   setWeather(data: any): void;
   // setIsLoaded: void;
 }) {
-  const [getForecast, setGetForecast] = useState([]);
+  const [getForecast, setGetForecast] = useState<any>();
 
   const forecastDays = {};
   const searchWeather = () => {
@@ -19,12 +20,12 @@ function Forecast(props: {
     };
 
     fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=462eaa5d277d46b0957222907232503&q=${props.searchedCity}&days=10&aqi=no&alerts=no`,
+      `https://api.weatherapi.com/v1/forecast.json?key=462eaa5d277d46b0957222907232503&q=${props.searchedCity}&days=4&aqi=no&alerts=no`,
       options
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        console.log(data, "hi");
         setGetForecast(data);
         // props.setIsLoaded(true);
       })
@@ -41,6 +42,22 @@ function Forecast(props: {
       10 Day Forecast
       <hr></hr>
       <p>{props.weather.forecast}</p>
+      <p>
+        {getForecast.forecast.forecastday.map((day: any) => (
+          <span className="bar-grid" style={{ margin: "0 2px" }}>
+            {day.hour.map((hour: any) => (
+              <div
+                style={{
+                  display: "inline-block",
+                  height: hour.temp_f + "px",
+                  width: "4px",
+                  backgroundColor: "white",
+                }}
+              ></div>
+            ))}
+          </span>
+        ))}
+      </p>
       {/* {props.weather.forecast.map} */}
     </div>
   );
